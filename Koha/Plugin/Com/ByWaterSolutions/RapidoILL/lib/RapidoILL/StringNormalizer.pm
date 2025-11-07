04094d9476ff7aab562c33f8ea5433baf77dbb50
Tomás Cohen Arazi <tomascohen@theke.io>
2025-11-07 10:38:34 -0300

    [#129] Update license license in all files
    Signed-off-by: Tomás Cohen Arazi <tomascohen@theke.io>

--- Koha/Plugin/Com/ByWaterSolutions/RapidoILL/lib/RapidoILL/StringNormalizer.pm
+++ Koha/Plugin/Com/ByWaterSolutions/RapidoILL/lib/RapidoILL/StringNormalizer.pm
@@ -1,19 +1,21 @@
 package RapidoILL::StringNormalizer;
 
-# This program is free software: you can redistribute it and/or modify
+# Copyright 2025 ByWater Solutions
+#
+# This file is part of The Rapido ILL plugin.
+#
+# The Rapido ILL plugin is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+# The Rapido ILL plugin is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# This program comes with ABSOLUTELY NO WARRANTY;
+# along with The Rapido ILL plugin; if not, see <https://www.gnu.org/licenses>.
 
 use Modern::Perl;
 
